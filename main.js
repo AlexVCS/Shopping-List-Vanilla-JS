87682f19783cb5b97fc6310431a460c479f3a313
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-11-26 10:32:22 -0500

    Updating addToList function to append li to the DOM

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-addtoList = () => {
+
+
+addToList = () => {
     const submitButton = document.getElementById('submit');
     const list = document.getElementById('list');
 
     submitButton.addEventListener("click", moreToBuy = () => {
         let item = document.getElementById("add-item").value;
         let addLi = document.createElement("LI");
-        let addItem = document.createTextNode(item);
+        addLi.textContent = item;
+        let addItem = document.querySelector('#list');
+        addItem.appendChild(addLi);
         list.appendChild(addItem);
     })
 }
 
-addtoList();
+addToList();
