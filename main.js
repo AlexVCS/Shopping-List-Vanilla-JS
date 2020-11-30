5328e52e74cb10e7b3bc084caf1ae7e3d6d3fde8
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-11-29 20:58:14 -0500

    Working on list

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addToList = () => {
     let item = document.getElementById("add-item").value;
 
     submitButton.addEventListener("click", moreToBuy = () => {
-        let addItem = document.querySelector("#list");
+        let addItem = document.getElementById("list");
         let addLi = document.createElement("LI");
         addLi.textContent = item;
         addItem.appendChild(addLi);
