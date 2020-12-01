9c29d628d4f5652ab344e4a7349b518f67a4f090
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-11-30 19:22:07 -0500

    Updating shopping list to empty input field after clicking add

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 addToList = () => {
     const submitButton = document.getElementById("submit");
-    let item = document.getElementById("add-item").value;
+    const item = document.getElementById("add-item");
 
-    submitButton.addEventListener("click", moreToBuy = () => {
-        let addItem = document.getElementById("list");
-        let addLi = document.createElement("LI");
-        addLi.textContent = item;
+    submitButton.addEventListener("click", moreToBuy = (e) => {
+        e.preventDefault();
+        const addItem = document.getElementById("list");
+        const addLi = document.createElement("LI");
+        addLi.textContent = item.value;
         addItem.appendChild(addLi);
+        item.value = "";
+        // checkForItem();
     });
+
+    // checkForItem = () => {
+    //     const itemValue = item.value.trim();
+
+    //     if (itemValue === '') {
+    //         setErrorFor(item, 'Please enter an item')
+    //     }
+    // }
+    // setErrorFor = (input, message) => {
+    //     const formControl = input.parentElement;
+    //     const small = formControl.querySelector('small');
+    //     small.innerText = message;
+    //     formControl.className = 'form-control error';
+    // }
 }
 
 addToList();
